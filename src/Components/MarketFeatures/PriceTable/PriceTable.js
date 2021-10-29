290c9cf85efd8a6aaa367f74c8cccb7373838fe0
EmmanuelObi <63916312+EmmanuelObi@users.noreply.github.com>
2021-10-29 19:55:01 +0100

    added background for values

--- src/Components/MarketFeatures/PriceTable/PriceTable.js
+++ src/Components/MarketFeatures/PriceTable/PriceTable.js
@@ -1,7 +1,8 @@
 import React from "react";
 import "./PriceTable.css";
 
-const PriceTable = ({ pColor, value }) => {
+const PriceTable = ({ pColor, lv, lvColor }) => {
+  const arrayOfValues = [10, 3.8, 9, 1, 7.5, 2];
   return (
     <div className="price-table">
       <thead>
@@ -10,7 +11,36 @@ const PriceTable = ({ pColor, value }) => {
         <th>lorem</th>
       </thead>
       <tbody style={{ borderColor: pColor }}>
-        <div>
+        {arrayOfValues.map((value) => {
+          return (
+            <div>
+              <td style={{ color: pColor }}>{lv}</td>
+              <td>{value}</td>
+              <td
+                className="td"
+                style={{
+                  width: "35%",
+                  textAlign: "right",
+                  display: "flex",
+                  justifyContent: "flex-end",
+                }}
+              >
+                <div
+                  className="td-div"
+                  style={{
+                    width: `${value * 10}%`,
+                    backgroundColor: lvColor,
+                    display: "flex",
+                    justifyContent: "flex-end",
+                  }}
+                >
+                  {value}
+                </div>
+              </td>
+            </div>
+          );
+        })}
+        {/* <div>
           <td style={{ color: pColor }}>{value}</td>
           <td>{value}</td>
           <td>{value}</td>
@@ -34,12 +64,7 @@ const PriceTable = ({ pColor, value }) => {
           <td style={{ color: pColor }}>{value}</td>
           <td>{value}</td>
           <td>{value}</td>
-        </div>
-        <div>
-          <td style={{ color: pColor }}>{value}</td>
-          <td>{value}</td>
-          <td>{value}</td>
-        </div>
+        </div> */}
       </tbody>
     </div>
   );
